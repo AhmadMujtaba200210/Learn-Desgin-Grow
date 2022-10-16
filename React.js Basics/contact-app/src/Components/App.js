29cd5b14a780e8930c8feb8cacffd96ac6219624
Ahmad Mujtaba <cadeta6@gmail.com>
2022-10-16 10:00:25 +0500

    build{/contact-app}:setup the file & folder structure

---
+++ React.js Basics/contact-app/src/Components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./App.css";
+import Header from "./Header";
+import AddContact from "./AddContact";
+import ContactList from "./ContactList";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      {/* <AddContact />
+      <ContactList /> */}
+    </div>
+  ); 
+}
+
+export default App;
