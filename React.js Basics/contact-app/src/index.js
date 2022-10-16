e6326fcec9e253d3a5066c38e53d1434bd1ba4f2
Ahmad Mujtaba <cadeta6@gmail.com>
2022-10-16 18:49:18 +0500

    config{index.js}: removed depreacted rendering

--- React.js Basics/contact-app/src/index.js
+++ React.js Basics/contact-app/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './Components/App';
-
-ReactDOM.render(
+const root=ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
